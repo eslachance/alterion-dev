a7ad43318e5834a7ffc4c9ded70d1b9a05a2890d
Evelyne Lachance <eslachance@gmail.com>
2024-05-28 13:37:50 -0400

    Fix missing unocss config lines

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "@solidjs/start/config";
 import UnoCSS from "unocss/vite";
 
 export default defineConfig({
+  vite: {
+    plugins: [UnoCSS()]
+  },
   server: {
     preset: "cloudflare-pages",
 
