a58b355082cb0d1aeced34ecfe53407d1b407aab
Evelyne Lachance <eslachance@gmail.com>
2024-05-28 21:22:02 -0400

    add basic content

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,44 +1,36 @@
-import { A } from "@solidjs/router";
-import Counter from "~/components/Counter";
-
 export default function Home() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
       <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
         <div class="flex justify-center items-center">
-          <span class="mr-5">Hello</span> <img class="w-12 h-12" src="https://unocss.dev/logo.svg" alt="UnoCSS logo" />!
+          <span class="mr-5">Alterion Development</span>
         </div>
       </h1>
-      <Counter />
-      <p class="mt-8">
-        Visit{" "}
-        <a
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="mt-2">
-        Visit{" "}
+      <h2 class="max-6-xs text-4xl">Under ... well... development, really.</h2>
+
+      <div class="flex flex-col justify-center items-center gap-2 pt-8">
+        <h3 class="text-2xl">Built With</h3>
         <a
           href="https://unocss.dev"
           target="_blank"
-          class="text-sky-600 hover:underline"
+          class="text-sky-600 hover:underline flex justify-center items-center gap-2 border-1 border-solid border-sky-600 rounded-md p-2"
         >
-          unocss.dev
-        </a>{" "}
-        to learn how to style your app.
-      </p>
-      <p class="my-4">
-        <span>Home</span>
-        {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </A>{" "}
-      </p>
+          <span>UnoCSS</span>
+          <img
+            class="w-6 h-6"
+            src="https://unocss.dev/logo.svg"
+            alt="UnoCSS logo"
+          />
+        </a>
+        <a
+          href="https://solidjs.com"
+          target="_blank"
+          class="text-sky-600 hover:underline flex justify-center items-center gap-2 border-1 border-solid border-sky-600 rounded-md p-2"
+        >
+          <span>SolidJS</span>
+          <img class="w-6 h-6" src="/solidjs.svg" alt="SolidJS logo" />
+        </a>
+      </div>
     </main>
   );
 }
